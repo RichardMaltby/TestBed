057748dc4c75253ca00a20d7ea38cc891f7bb166
Richard Maltby <rlmaltby@gmail.com>
2018-05-29 19:43:23 -0700

    commiting solution in progress

---
+++ Mp3HexInserter/Mp3HexInserter.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Mp3HexInserter
+{
+    partial class Mp3HexInserter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mp3HexInserter));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chooseFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.GetMp3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.AddCoin = new System.Windows.Forms.Button();
+            this.OutputText = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.StoreCoins = new System.Windows.Forms.Button();
+            this.GetCoins = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1199, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1199, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chooseFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // chooseFileToolStripMenuItem
+            // 
+            this.chooseFileToolStripMenuItem.Name = "chooseFileToolStripMenuItem";
+            this.chooseFileToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.chooseFileToolStripMenuItem.Text = "Choose File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 534);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1199, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "Mp3Hex";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.GetMp3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1199, 94);
+            this.panel1.TabIndex = 4;
+            // 
+            // GetMp3
+            // 
+            this.GetMp3.AccessibleDescription = "Select the Mp3 file to be used";
+            this.GetMp3.AccessibleName = "Get Mp3";
+            this.GetMp3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GetMp3.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.GetMp3.Location = new System.Drawing.Point(35, 30);
+            this.GetMp3.Name = "GetMp3";
+            this.GetMp3.Size = new System.Drawing.Size(101, 45);
+            this.GetMp3.TabIndex = 0;
+            this.GetMp3.Text = "Mp3 File";
+            this.GetMp3.UseVisualStyleBackColor = true;
+            this.GetMp3.Click += new System.EventHandler(this.GetMp3_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel2.Controls.Add(this.AddCoin);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 142);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1199, 94);
+            this.panel2.TabIndex = 5;
+            // 
+            // AddCoin
+            // 
+            this.AddCoin.AccessibleDescription = "Select a coin/coins to add to the MP3";
+            this.AddCoin.AccessibleName = "Add Coin";
+            this.AddCoin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddCoin.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.AddCoin.Location = new System.Drawing.Point(35, 20);
+            this.AddCoin.Name = "AddCoin";
+            this.AddCoin.Size = new System.Drawing.Size(101, 47);
+            this.AddCoin.TabIndex = 0;
+            this.AddCoin.Text = "Add Coin";
+            this.AddCoin.UseVisualStyleBackColor = true;
+            this.AddCoin.Click += new System.EventHandler(this.AddCoin_Click);
+            // 
+            // OutputText
+            // 
+            this.OutputText.AccessibleDescription = "OutPut field";
+            this.OutputText.AccessibleName = "OutputText";
+            this.OutputText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutputText.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.OutputText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputText.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.OutputText.Location = new System.Drawing.Point(0, 90);
+            this.OutputText.MaxLength = 10000;
+            this.OutputText.Multiline = true;
+            this.OutputText.Name = "OutputText";
+            this.OutputText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputText.Size = new System.Drawing.Size(1199, 208);
+            this.OutputText.TabIndex = 6;
+            this.OutputText.Text = "Here is some filler text";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.OutputText);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 236);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1199, 298);
+            this.panel3.TabIndex = 6;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel4.Controls.Add(this.StoreCoins);
+            this.panel4.Controls.Add(this.GetCoins);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1199, 91);
+            this.panel4.TabIndex = 7;
+            // 
+            // StoreCoins
+            // 
+            this.StoreCoins.AccessibleDescription = "Used to Get Coins stored in MP3 files";
+            this.StoreCoins.AccessibleName = "Get Coins";
+            this.StoreCoins.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StoreCoins.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.StoreCoins.Location = new System.Drawing.Point(311, 25);
+            this.StoreCoins.Name = "StoreCoins";
+            this.StoreCoins.Size = new System.Drawing.Size(183, 42);
+            this.StoreCoins.TabIndex = 1;
+            this.StoreCoins.Text = "Store in Mp3";
+            this.StoreCoins.UseVisualStyleBackColor = true;
+            this.StoreCoins.Click += new System.EventHandler(this.StoreCoins_Click);
+            // 
+            // GetCoins
+            // 
+            this.GetCoins.AccessibleDescription = "Used to Get Coins stored in MP3 files";
+            this.GetCoins.AccessibleName = "Get Coins";
+            this.GetCoins.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GetCoins.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.GetCoins.Location = new System.Drawing.Point(35, 25);
+            this.GetCoins.Name = "GetCoins";
+            this.GetCoins.Size = new System.Drawing.Size(183, 42);
+            this.GetCoins.TabIndex = 0;
+            this.GetCoins.Text = "Get from Mp3";
+            this.GetCoins.UseVisualStyleBackColor = true;
+            this.GetCoins.Click += new System.EventHandler(this.GetCoins_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // Mp3HexInserter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1199, 556);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.ForeColor = System.Drawing.SystemColors.Window;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Mp3HexInserter";
+            this.Text = "Mp3HexInserter";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chooseFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button GetMp3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button AddCoin;
+        private System.Windows.Forms.TextBox OutputText;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button GetCoins;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button StoreCoins;
+    }
+}
+
---
+++ Mp3HexInserter/Mp3HexInserter.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/// <summary>
+/// Mp3HexInserter,
+/// 
+/// select Mp3 save file path to Mp3FilePath;
+/// select CloudCoins save to CloudCoinFilePath;
+/// 
+/// 
+/// 
+/// 
+/// </summary>
+namespace Mp3HexInserter
+{
+    public partial class Mp3HexInserter : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        private static readonly int CHUNK_SIZE = 1024;
+        string Mp3FilePath;
+        string CloudCoinFilePath;
+        byte[] bytes = { };
+
+        public Mp3HexInserter()
+        {
+            InitializeComponent();
+        }
+
+        //Retrieve an Mp3 byte file
+        //Iterate through the file searching for CloudCoin Markers,
+        //When a marker is found push bytes to array.
+        private void GetCoins_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Retrieve a Mp3 byte file.
+        //Retrieve the CloudCoin byte file.
+        //Parse Mp3 frames
+        //Insert CloudCoin markers
+        //Append CloudCoin to markers
+        private void StoreCoins_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //On clicking 'Mp3 File' Button displays browse window, 
+        //Filters contents to Mp3 only,
+        //Calls Function to save the file path.
+        private void GetMp3_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Mp3|*.mp3";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                SetMp3FilePath(ofd.FileName);
+            
+            };
+        }
+
+        //On clicking 'Add Coin' Button displays browse window, 
+        //Filters contents to .txt only,
+        //Calls Function to save the file path.
+        private void AddCoin_Click(object sender, EventArgs e)
+        {
+            {
+                ofd.Filter = "Text|*.txt";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    SetCloudCoinFilePath(ofd.FileName);
+                };
+            }
+        }
+
+        //Called when saving a new Mp3 file-path.
+        public void SetMp3FilePath(string Mp3FilePath_)
+        {
+            Mp3FilePath = Mp3FilePath_;
+            OutputText.Text = "MP3 file-path saved!";
+            DumpMp3(Mp3FilePath);
+        }
+
+        //Called when saveing a new CloudCoin file-path.
+        public void SetCloudCoinFilePath(string CloudCoinFilePath_)
+        {
+            CloudCoinFilePath = CloudCoinFilePath_;
+            OutputText.Text = "CloudCoin file-path saved!";
+        }
+
+        public void DumpMp3(string filePath)
+        {
+            string path = filePath;
+            Console.WriteLine(path);
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using (BinaryReader br = new BinaryReader(fs, new ASCIIEncoding()))
+                {
+                    byte[] chunk;
+                    OutputText.AppendText(Environment.NewLine);
+                   
+                    chunk = br.ReadBytes(CHUNK_SIZE);
+                    while (chunk.Length > 0)
+                    {
+                        OutputText.AppendText(Environment.NewLine);
+                        DumpBytes(chunk, chunk.Length);
+                        chunk = br.ReadBytes(CHUNK_SIZE);
+                    }
+                }
+            }
+            
+        }
+
+        public void DumpBytes(byte[] bdata, int len)
+        {
+            int i;
+            int j = 0;
+            char dchar;
+
+            // 3 * 16 chars for hex display, 16 chars for text and 8 chars
+            // for the 'gutter' int the middle.
+            StringBuilder dumptext = new StringBuilder("         ", len + 8);
+
+            for (i = 0; i < len; i++)
+            {
+                dumptext.Insert(j * 3, String.Format("{0:X} ", (int)bdata[i]));
+                dchar = (char)bdata[i];
+
+                //' replace 'non-printable' chars with a '.'.
+                if (Char.IsWhiteSpace(dchar) || Char.IsControl(dchar))
+                {
+                    dchar = '-';
+                }
+                dumptext.Append(dchar);
+                j++;
+
+                if (j == 16)
+                {
+                    OutputText.AppendText(dumptext.ToString());
+                    dumptext.Length = 0;
+                    dumptext.Append("        ");
+                    j = 0;
+                }
+
+            }
+
+            // display the remaining line
+            if (j > 0)
+            {
+                for (i = j; i < 16; i++)
+                {
+                    dumptext.Insert(j * 3, "   ");
+                }
+                OutputText.AppendText(dumptext.ToString());
+            }
+            
+        }
+        //       //Using the saved file paths to create a byte array. 
+        //       private void ConvertToByteArr()
+        //       {
+        //
+        //       }
+    }
+}
